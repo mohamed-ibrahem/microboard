c2b9a7b67a1d6a92b4bae5a18e55e37f1f91747e
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2020-08-17 21:28:43 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -28,5 +28,22 @@ return [
     'middleware' => [
         'web',
         //
-    ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Microboard Views configurators.
+    |--------------------------------------------------------------------------
+    |
+    | These middleware will be assigned to every Microboard route, giving you the
+    | chance to add your own middleware to this stack or override any of
+    | the existing middleware. Or, you can just stick with this stack.
+    |
+    */
+
+    'view' => [
+        'enable_notifications' => true,
+        'enable_global_search' => true,
+        'enable_breadcrumbs' => true
+    ],
 ];
